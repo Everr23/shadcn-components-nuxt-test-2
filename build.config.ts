0eba2023736261f5f00ee0514a502015d6e9a71f
Ever Vega Suazo <ever.vega.suazo@una.cr>
2025-06-09 16:00:58 -0600

    test delete option from build config

--- build.config.ts
+++ build.config.ts
@@ -1,10 +1,5 @@
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
-  hooks: {
-    "mkdist:entry:options"(ctx, entry, options) {
-      options.addRelativeDeclarationExtensions = false;
-    },
-  },
   externals: ["#build/unaxt", "vite"],
 });
